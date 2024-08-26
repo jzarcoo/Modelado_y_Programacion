e04ff7a019c9adf3ac651b27c2ebb7e627d58013
jzarcoo <zarco_romero_jose_antonio@comunidad.unam.mx>
2024-08-26 14:39:00 -0600

    delete Proyecto02

--- Proyecto02/proyecto2/bin/servidor-proyecto2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-DIR="$(dirname $0)"
-
-mvn -q exec:java             \
-    -f "${DIR}/../pom.xml"   \
-    -Dexec.mainClass=mx.unam.ciencias.modelado.proyecto2.ServidorProyecto2 -Dexec.args="$*"
--- Proyecto02/proyecto2/src/main/java/mx/unam/ciencias/modelado/proyecto2/InterfazAdministrador.java
+++
@@ -1,8 +0,0 @@
-package mx.unam.ciencias.modelado.proyecto2;
-
-/**
- * <p>Interfaz que define a un administrador.</p>
- */
-public interface InterfazAdministrador extends InterfazUsuario {
-
-}
